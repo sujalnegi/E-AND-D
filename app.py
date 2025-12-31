c1fe3027a7c60d25e564794d578960b82e95fe37
sujalnegi <sujal1negi@gmail.com>
2025-12-31 20:24:19 +0530

    Shifted to tailwind css, made encryption and decryption pages and added formfields

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/encryption')
+def encryption():
+    return render_template('encryption.html')
+
+@app.route('/encrypt_text', methods=['POST'])
+def encrypt_text():
+    return "Encryption Logic Not Implemented Yet"
+
+@app.route('/decryption')
+def decryption():
+    return render_template('decryption.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
